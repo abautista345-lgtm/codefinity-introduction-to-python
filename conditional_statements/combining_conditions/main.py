d235ed2703d2fd980351d743548a183bc9d92e48
abautista345-lgtm <>
2025-12-15 22:02:13 +0000

    Combining Conditions

--- conditional_statements/combining_conditions/main.py
+++ conditional_statements/combining_conditions/main.py
@@ -1,3 +1,7 @@
 # The item's discount and stock status have been defined
 discounted = False
 lowStock = True
+movingProduct = (discounted == False) or (lowStock == True)
+promotion = not(movingProduct)
+
+print("Is the item eligible for promotion?", promotion)
